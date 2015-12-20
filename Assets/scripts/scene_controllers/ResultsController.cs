6c6da4921d48a18074535c89aff13f0b23076ded
Atra <sharr@derpymail.org>
2015-12-19 23:15:09 -0500

    resolve issue #13

--- Assets/scripts/scene_controllers/ResultsController.cs
+++ Assets/scripts/scene_controllers/ResultsController.cs
@@ -9,13 +9,31 @@ public class ResultsController : MonoBehaviour {
     public const string NEW_RECORD_TEXT = "Broke previous record of {0}!";
     public const string FIRST_RECORD_TEST = "On the board.";
 
+    public const string WAITING_TEXT = "CAN RETURN IN {0:F1}";
+    public const string DONE_TEXT = "RETURN TO MODE SELECT";
+    public const float RETURN_DELAY = 1.0f;
+    public float currentDelay;
+
+    Text gameOverTitle;
+    Text scoreTitle;
+    Text lossMessage;
+    Text score;
+    Text newRecordMessage;
+    Button returnButton;
+
     // Use this for initialization
     void Start () {
-        Text gameOverTitle = GameObject.Find("GameOver").GetComponent<Text>();
-        Text scoreTitle = GameObject.Find("Score").GetComponent<Text>();
-        Text lossMessage = GameObject.Find("LossReason").GetComponent<Text>();
-        Text score = GameObject.Find("Count").GetComponent<Text>();
-        Text newRecordMessage = GameObject.Find("Record").GetComponent<Text>();
+        currentDelay = RETURN_DELAY;
+
+        gameOverTitle = GameObject.Find("GameOver").GetComponent<Text>();
+        scoreTitle = GameObject.Find("Score").GetComponent<Text>();
+        lossMessage = GameObject.Find("LossReason").GetComponent<Text>();
+        score = GameObject.Find("Count").GetComponent<Text>();
+        newRecordMessage = GameObject.Find("Record").GetComponent<Text>();
+        returnButton = GameObject.Find("Return").GetComponent<Button>();
+
+        returnButton.interactable = false;
+
         newRecordMessage.text = "";
 
         gameOverTitle.text = PlayerPrefs.GetString(Game.GAME_MODE_LOCATION).ToUpper() + GAME_OVER_APPEND;
@@ -71,6 +89,11 @@ public class ResultsController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+	    if ((currentDelay -= Time.deltaTime) < 0) {
+            returnButton.interactable = true;
+            returnButton.GetComponentInChildren<Text>().text = DONE_TEXT;
+        } else {
+            returnButton.GetComponentInChildren<Text>().text = string.Format(WAITING_TEXT, currentDelay);
+        }
 	}
 }
