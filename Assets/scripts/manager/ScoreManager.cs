eedb9993844d2dd4f5716c4cfce2a16305e8d5e6
Atra <sharr@derpymail.org>
2016-01-20 12:21:23 -0500

    Created new apk

--- Assets/scripts/manager/ScoreManager.cs
+++ Assets/scripts/manager/ScoreManager.cs
@@ -8,18 +8,18 @@ using UnityEngine.Assertions;
  */
 public class ScoreManager : MonoBehaviour {
 	int value; //Number that will be displayed by the score text
-	
+
 	// Use this for initialization
 	void Start () {
 		Assert.IsNotNull(gameObject.GetComponent<Text>().text);
 
 		gameObject.GetComponent<Text>().text = "null";
 	}
-	
+
 	public void setValue(int newValue) {
 		this.value = newValue;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		gameObject.GetComponent<Text>().text = "" + value;
